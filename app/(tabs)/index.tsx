ebc132c616283a90fde910bdfde9dc6b698e2806
Maxamed-Maxamed <20088349@mail.wit.ie>
2025-01-02 14:49:56 +0000

    add image background to app screen and update TypeScript configuration for image modules

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,46 @@
-import { View, Text , StyleSheet} from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import React from 'react';
+
+const iceCoffeeImg = require('@/assets/images/iced-coffee.png');
 
 const app = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text} >App coffe shop </Text>
-    </View>
-  )
-}
+      <ImageBackground 
+        source={iceCoffeeImg} 
+        resizeMode='cover'
+        style = {styles.image}
 
-export default app
+     >
+        <Text style={styles.text}>App coffee shop</Text>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default app;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-   
   text: {
     fontSize: 20,
     fontWeight: 'bold',
     color: 'red',
     textAlign: 'center',
     marginTop: 20,
+  },
+
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    resizeMode: 'cover',
+    
+    
   }
-})
+});
