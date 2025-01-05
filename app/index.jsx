2e3fa4ebc1c428c318d42901b2e934553c4202be
Maxamed-Maxamed <20088349@mail.wit.ie>
2025-01-05 17:10:50 +0000

    refactor navigation structure by removing old explore tab and adding contact screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,33 +1,30 @@
-import { View, Text, StyleSheet, ImageBackground , Pressable, Button} from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Pressable } from 'react-native';
 import { Link } from 'expo-router';
 import React from 'react';
 
 import iceCoffeeImg from '@/assets/images/iced-coffee.png';
 
-const app = () => {
+const App = () => {
   return (
     <View style={styles.container}>
       <ImageBackground 
         source={iceCoffeeImg} 
-        resizeMode='cover'
-        style = {styles.image}
-
-     >
-        <Text style={styles.title}>App coffee shop</Text>
-        <Link href='/explore' style={{marginHorizontal : 'auto'}} asChild> 
-        <Pressable style={styles.button}>
-          <Text style={styles.buttonText}>
-            Go to Explore pages
-
-          </Text>
+        style={styles.image}
+      >
+        <Text style={styles.title}>App Coffee Shop</Text>
+        <Link href='/contact' asChild>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>
+              Go to Contact Us Page
+            </Text>
           </Pressable>
-         </Link>
+        </Link>
       </ImageBackground>
     </View>
   );
 };
 
-export default app;
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -44,23 +41,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     marginBottom: 20,
   },
-  link: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-    textAlign: 'center',
-    marginTop: 20,
-    textDecorationLine: 'underline',
-    padding: 10,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
   button: {
     height: 50,
     borderRadius: 25,
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
-    padding:6,
+    padding: 6,
     justifyContent: 'center',
-    
   },
   buttonText: {
     fontSize: 16,
@@ -69,14 +55,10 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 4,
   },
-
   image: {
     width: '100%',
     height: '100%',
     flex: 1,
     justifyContent: 'center',
-    resizeMode: 'cover',
-    
-    
-  }
+  },
 });
